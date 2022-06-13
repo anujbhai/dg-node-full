3a7508726645c68d990a51ce556b15b5f3acc884
Anuj Upadhyay <anuj.upadhyay@rsystems.com>
2022-06-13 17:01:40 +0530

    dg-node-full - finished files.

--- files/stream.js
+++ files/stream.js
@@ -1,3 +1,12 @@
 const fs = require("fs");
 
 const rs = fs.createReadStream("./files/lorem.txt", {encoding: "utf-8"});
+
+const ws = fs.createWriteStream("./files/new-lorem.txt");
+
+/* rs.on("data", (dataChunk) => {
+	ws.write(dataChunk);
+}); */
+
+rs.pipe(ws);
+
