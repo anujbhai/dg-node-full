11d03b365580ede75f15c530ea4cfa77b43d383f
Anuj Upadhyay <anuj.upadhyay@rsystems.com>
2022-07-04 21:52:31 +0530

    dg-node-full - 09 MVC REST API in ExpressJS.

---
+++ dgnode-09-MVC-REST-API/server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
+const {logger} = require("./middleware/logEvents");
+const errorHandler = require("./middleware/errorHandler");
+
+const PORT = process.env.PORT || 3500;
+
+// custom middleware logger
+app.use(logger);
+
+// Cross origin resource sharing
+app.use(cors(corsOptions));
+
+// Built-in middleware to handle urlencoded data
+app.use(express.urlencoded({extended: false}));
+
+// Built-in middleware for json
+app.use(express.json());
+
+// serve static files
+app.use("/", express.static(path.join(__dirname, "/public")));
+
+app.use("/", require("./routes/root"));
+app.use("/employees", require("./routes/api/employees"));
+
+app.all("/*", (req, res) => {
+	res.status(404);
+	if (req.accepts("html")) {
+		res.sendFile(path.join(__dirname, "views", "404.html"));
+	} else if (req.accepts("json")) {
+		res.json({error: "404 Not Found"});
+	} else {
+		res.type("txt").send("404 Not FOund");
+	}
+});
+
+// Error handling in express middleware
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
