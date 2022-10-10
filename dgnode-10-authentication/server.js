16e2e169b6309cf67d7853d3eee4937e3acefddc
ANUJ UPADHYAY <anuj.upadhyay@rsystems.com>
2022-10-11 01:11:30 +0530

    dgnode-11.2-JWT-Auth-revisit - done.

--- dgnode-10-authentication/server.js
+++ dgnode-10-authentication/server.js
@@ -5,11 +5,17 @@ const cors = require("cors");
 const corsOptions = require("./config/corsOptions");
 const {logger} = require("./middleware/logEvents");
 const errorHandler = require("./middleware/errorHandler");
-
+const verifyJWT = require("./middleware/verifyJWT");
+const cookieParser = require("cookie-parser");
+const credentials = require("./middleware/credentials");
 const PORT = process.env.PORT || 3500;
 
 // custom middleware logger
 app.use(logger);
+
+// Handle options credentials check - before CORS!
+// and fetch cookies credentials requirement
+app.use(credentials);
 
 // Cross origin resource sharing
 app.use(cors(corsOptions));
@@ -20,12 +26,19 @@ app.use(express.urlencoded({extended: false}));
 // Built-in middleware for json
 app.use(express.json());
 
+// middleware for cookies
+app.use(cookieParser());
+
 // serve static files
 app.use("/", express.static(path.join(__dirname, "/public")));
 
 app.use("/", require("./routes/root"));
 app.use("/register", require("./routes/register"));
 app.use("/auth", require("./routes/auth"));
+app.use("/refresh", require("./routes/refresh"));
+app.use("/logout", require("./routes/logout"));
+
+app.use(verifyJWT);
 app.use("/employees", require("./routes/api/employees"));
 
 app.all("/*", (req, res) => {
